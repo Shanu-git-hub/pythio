360333956f9de8a42ab34abc486cd9b644f109c7
Shanu-git-hub <shanubansal4531@gmail.com>
2025-01-20 14:15:44 +0530

    Add files via upload

---
+++ Shanu.python.py
@@ -0,0 +1,8 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 12:49:59) [MSC v.1935 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> print("gcdd of 24,60 is:")
+gcdd of 24,60 is:
+>>> print(math.gcd(24,60))
+12
+>>> 
